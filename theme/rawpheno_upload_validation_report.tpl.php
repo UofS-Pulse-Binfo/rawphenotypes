94895ffac74cdf56294ac69ccd2bf1a64c25a408
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2016-03-07 09:28:28 -0600

    Added hook for file validation. working in isolation but not attached to form yet

--- theme/rawpheno_upload_validation_report.tpl.php
+++ theme/rawpheno_upload_validation_report.tpl.php
@@ -3,23 +3,23 @@
  *
  */
 
-$messages = array(
-  'is_xlsx' => 'File format is XLSX',
-  'tab_exists' => '"Measurements" tab exists and has data',
-  'missing_header' => '"Plot", "Entry", "Name", "Rep" and "Location" columns exists.',
-);
+// Get information about the validators available in oder to theme the validation results.
+$validators = module_invoke_all('rawpheno_validators');
 ?>
 
 <h3>Validation Results</h3>
 <ul>
 <?php foreach ($status as $key => $result) {
   $class = ($result == TRUE) ? 'success' : 'failed';
-  print '<li class="' . $class . '">' . $messages[$key] . '</li>';
+  print '<li class="' . $class . '">' . $validators[$key]['label'] . '</li>';
   
   // Provide further information if the validation failed.
   if ($result !== TRUE) {
-    if ($key == 'missing_header') {
-      print '<ul><li>The following required columns are missing: "' . implode('", "', $result) . '".</li></ul>';
+    if (isset($validators[$key]['message callback']) AND function_exists($validators[$key]['message callback'])) {
+      $messages = call_user_func($validators[$key]['message callback'], $result);
+      if (!empty($messages)) {
+        print '<ul><li>' . implode('</li><li>', $messages) . '</li></ul>';
+      }
     }
   }
 }?>
