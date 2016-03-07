b0c5a66c425bb7052e233c01f4968d0e47313fdf
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2016-03-07 12:07:21 -0600

    cleaned up merge conflict that was missed :-(

--- theme/js/rawpheno.upload.stage01.js
+++ theme/js/rawpheno.upload.stage01.js
@@ -5,8 +5,21 @@
 (function($) {
   Drupal.behaviors.rawphenoToggleDragnDropMessage = {
     attach: function (contex, settings) {
-      $(document).ready(function() { 
 
+        // Hide the next button unless needed.
+        var submitButton = $('#edit-next-step');
+        var successMsg = $('#rawpheno-upload-successful');
+        
+        if (successMsg.length) {
+          submitButton.removeClass('form-button-disabled');
+          submitButton.removeAttr('disabled');
+         }
+        else {
+          submitButton.addClass('form-button-disabled');
+          submitButton.attr('disabled','disabled');
+        }
+        
+        // Handle the hover-over effects of the drop-zone.
 		    $('.droppable-browse-button').text('choose your file');
 		    // Drag over events.
 		    var drop = document.getElementById('droppable-dnd');
@@ -15,7 +28,6 @@
         var dropMessageHTML = $('.droppable-message').html();
         var dropMessage = $('.droppable-message');
         
-<<<<<<< Updated upstream
         if (drop) {
           drop.addEventListener("dragover", function() { 
             drop.style.border = '1px dashed #999999';
@@ -38,51 +50,6 @@
             dropMessage.html(dropMessageHTML);
           });
         }
-      });
-    }
-  };
-}(jQuery));
-
-/**
- * Diable the next step button until a success message with an id=rawpheno-upload-successful
- * appears on the page.
- */
-(function($) {
-  Drupal.behaviors.rawphenoUpload1ControlWorkflow = {
-    attach: function (contex, settings) {
-      $(document).ready(function() { 
-
-        var submitButton = $('#edit-next-step');
-        var successMsg = $('#rawpheno-upload-successful');
-        
-        if (successMsg.length) {
-          submitButton.removeClass('form-button-disabled');
-          submitButton.removeAttr('disabled');
-        }
-        else {
-          submitButton.addClass('form-button-disabled');
-          submitButton.attr('disabled','disabled');
-        }
-=======
-        drop.addEventListener("dragover", function() { 
-          dropInner.style.border = '4px dashed #999999';
-          // Create a new instruction to user.
-          dropMessage.text('Drop to upload and validate your spreadsheet'); 
-        });  
-      
-        drop.addEventListener("dragleave", function() { 
-          dropInner.style.border = 'none';
-          // Remove new instruction and restore original message.
-          dropMessage.html(dropMessageHTML);
-        });
-        
-        drop.addEventListener("drop", function() { 
-          dropInner.style.border = 'none';
-          // Remove new instruction and restore original message.
-          dropMessage.html(dropMessageHTML);
-        });
->>>>>>> Stashed changes
-      });
     }
   };
 }(jQuery));
