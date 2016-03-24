7f7e8f373bfaa59b30166cb95f72814c03c026b1
Reynold Tan <reynoldltan@gmail.com>
2016-03-24 16:33:15 -0600

    03.24.2016 - Updates to upload
    - Animate next button.
    - Updated validation result theme.
    - Removed default values in data collection spreadsheet.

--- theme/js/rawpheno.upload.script.js
+++ theme/js/rawpheno.upload.script.js
@@ -1,9 +1,11 @@
 /**
  * @file
  * Allow user to collapse upload instructions message box and other windows.
+ * Notify user to click next step button when does not response to a stage in
+ * upload for a specific length of time.
  */
 (function($) {
-  Drupal.behaviors.rawphenoToggleWindows = {
+  Drupal.behaviors.rawphenoPageElementBehaviors = {
     attach: function (context, settings) {
       // Allow user to collapse upload information window.
       $('hr.button-collapse-infowindow').click(function() {
@@ -25,6 +27,24 @@
           });
         }
       }); 
+      
+      // If after 10 seconds and no response from user,
+      // button will start to change color and blink.
+      var nextButton = $('#edit-next-step');
+      // Fieldset container of describe additional trait.
+      // If this fieldset is in DOM, don't apply button animation.
+      var fieldSet = ($('#edit-xls-review-fldset').length) ? 1 : 0;
+
+      if (nextButton.length > 0 && nextButton.is(':visible') && fieldSet == 0) { 
+        var i = 0;
+        var timer = setInterval(function() {
+          if (i > 10) { 
+            var bg = (i%2 == 0) ? '#F7F7F7' : '#DEDEDE';
+            nextButton.css({background: bg});
+          }
+          i++;
+        }, 1000);
+      }
     }
   };
 }(jQuery));
--- theme/rawpheno_upload_validation_report.tpl.php
+++ theme/rawpheno_upload_validation_report.tpl.php
@@ -11,21 +11,31 @@
 // Get information about the validators available in oder to theme the validation results.
 $validators = module_invoke_all('rawpheno_validators');
 ?>
-<div class="window-info">
-  <div id="window-validation-result">
-    <h3>Validation Result</h3>
+
+<fieldset class="collapsible">
+  <legend>
+    <span class="fieldset-legend">
+      <a href="#" class="fieldset-title">
+        <span class="fieldset-legend-prefix element-invisible">Hide</span>
+        Validation Result
+      </a>
+      <span class="summary"></span>
+    </span>
+  </legend>
+  <div class="fieldset-wrapper">
+    <br />
     <ul class="error-main">
       <?php 
         foreach ($status as $key => $result) {
           $class = ($result === TRUE) ? 'success' : 'failed';
           print '<li class="' . $class . '"><em>' . $validators[$key]['label'] . '</em></li>';
-  
+
           // Provide further information if the validation failed.
           if ($result !== TRUE) {
             if (isset($validators[$key]['message callback']) AND function_exists($validators[$key]['message callback'])) {
               $messages = call_user_func($validators[$key]['message callback'], $result);
               if (!empty($messages)) {
-                print '<ul class="error-detail"><li class="error-item">' . implode('</li><li class="error-item">', $messages) . '</li></ul>';
+                print '<ul class="error-detail"><li class="error-item">' . implode('</li><li class="error-item">', $messages) . '</li></ul><br />';
               }
             }
           }
@@ -33,6 +43,4 @@ $validators = module_invoke_all('rawpheno_validators');
       ?>  
     </ul>
   </div>
-  <br />
-</div>
-<hr class="button-collapse-infowindow window-on" />
+</fieldset>
