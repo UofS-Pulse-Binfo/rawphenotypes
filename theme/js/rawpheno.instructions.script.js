6e05fd19aefd4a156422024e1b4f88bf17bc65e4
Reynold Tan <reynoldltan@gmail.com>
2016-10-31 14:59:51 -0600

    10.31.2016
    - Fixed layout issue in instructions page when displayed in a fixed
    width layout

--- theme/js/rawpheno.instructions.script.js
+++ theme/js/rawpheno.instructions.script.js
@@ -5,30 +5,43 @@
 (function($) {
   Drupal.behaviors.rawphenoInstructionsTab = {
     attach: function (context, settings) {
+      // Remove context button link when parent container is less than 500px.
+      // Viewport media query is present in css to restructure layout on various screen width.
+      var parentContainer = $('div.container-page');
+      if (parentContainer && parentContainer.width() <= 720) {
+        // Hide context button.
+        if ($('.subtitle-right a') && $('.subtitle-right a').is(':visible')) {
+          $('.subtitle-right a').hide();
+        }
+
+        // Fix tabs stacking one on top of each other by reducing font-size
+        $('#tabs ul:first-child li').css('font-size', '0.70em');
+      }
+
       // IMAGE GALLERY
       // Array to hold all image file.
       var gallery = new Array(0, 1);
       // Array to hold all relevant captions.
       var caption = new Array(0, 1);
-    
+
       // Gallery elements set 1.
-      gallery[0] = ['01-tendrils-no-elongation', 
-                    '02-tendrils-no-elongation', 
+      gallery[0] = ['01-tendrils-no-elongation',
+                    '02-tendrils-no-elongation',
                     '01-tendrils-elongation'];
-    
-      caption[0] = ['No elongation', 
-                    'No elongation', 
+
+      caption[0] = ['No elongation',
+                    'No elongation',
                     'Elongation'];
-    
+
       // Gallery elements set 2.
-      gallery[1] = ['01-pods-emerged', 
-                    '02-pods-emerged', 
-                    '01-pods-variation', 
-                    '02-pods-variation']; 
-    
-      caption[1] = ['Freshly emerged pods', 
-                    'Freshly emerged pods', 
-                    'Sample of the pod variation', 
+      gallery[1] = ['01-pods-emerged',
+                    '02-pods-emerged',
+                    '01-pods-variation',
+                    '02-pods-variation'];
+
+      caption[1] = ['Freshly emerged pods',
+                    'Freshly emerged pods',
+                    'Sample of the pod variation',
                     'Sample of the pod variation'];
 
       // Path to appendix folder.
@@ -40,42 +53,42 @@
         var topic = $('#fragment-5 select').val();
         // Image container - showing default image.
         var curImg = $('#cur-img').val();
-      
+
         // Hold the index of the current image shown.
         curImg = parseInt(curImg);
-      
+
         // Determine if user clicks on next or prev link
         // in the image gallery.
         var showImg = ($(this).text() == '<') ? curImg - 1 : curImg + 1;
-      
+
         // Replace the image src to show the next or prev image in the topic.
         if (gallery[topic][showImg]) {
-          $('#fragment-5 img').attr('src', imgPath+gallery[topic][showImg]+'.jpg'); 
-          $('#fragment-5 em').contents().replaceWith(caption[topic][showImg]); 
+          $('#fragment-5 img').attr('src', imgPath+gallery[topic][showImg]+'.jpg');
+          $('#fragment-5 em').contents().replaceWith(caption[topic][showImg]);
           $('#cur-img').val(showImg);
         }
       });
-    
-      // When user will change topic, load default or first item in the 
-      // image and caption array.	
-      $('#fragment-5 select').change(function(){ 
+
+      // When user will change topic, load default or first item in the
+      // image and caption array.
+      $('#fragment-5 select').change(function(){
         var topic = $(this).val();
         $('#fragment-5 img').attr('src', imgPath+gallery[topic][0]+'.jpg');
-        $('#fragment-5 em').contents().replaceWith(caption[topic][0]); 
+        $('#fragment-5 em').contents().replaceWith(caption[topic][0]);
         $('#cur-img').val(0);
       });
-    
+
       // SEARCH FUNCTIONALITY
       // Reference to search textbox.
       var txtField = $('#edit-txt-search');
       // Reference to search buttons.
       var btnSearch = $("#btn_submit");
-    
+
       // Create a label as default value in the search text field.
       // When selected will clear the value.
-      txtField.focus(function() {  
-      if (this.value == 'Search Trait') { 
-        this.value = ''; 
+      txtField.focus(function() {
+      if (this.value == 'Search Trait') {
+        this.value = '';
       }}).blur(function() {
         if (this.value.trim() == '') {
         this.value = 'Search Trait';
@@ -83,17 +96,17 @@
       }});
 
       // Clear all search when user types in new
-      // keywords into the search field. 
+      // keywords into the search field.
       txtField.keypress(function() {
         removeElements();
       });
 
       // Initialize JQuery Tabs.
-      $('#tabs').tabs({delay:0}); 
-    
+      $('#tabs').tabs({delay:0});
+
       // Array of traits.
       var availableTrait = new Array();
-    
+
       // Access JSON List of traits created by function callback
       // in instructions page.
       var pathJSON = $('#traits-json').val();
@@ -102,14 +115,14 @@
           availableTrait[i] = field;
         })
       });
-    
+
       // Search button is clicked.
-      btnSearch.click(function() { 
+      btnSearch.click(function() {
         // Clear or reset previous search.
         removeElements();
 
         $('#container-search-result').addClass('search-on');
- 
+
         if (txtField.val() == null || txtField.val().trim() == '' || txtField.val().length <= 1) {
           // Search field is blank.
           var title = 'Invalid value in search field.';
@@ -121,7 +134,7 @@
           var traitsWithKey = new Array;
           // Get the index of the trait.
           var traitIndex = null;
-        
+
           for(var i = 0; i < availableTrait.length; i++) {
             if (availableTrait[i].toLowerCase() == txtField.val().toLowerCase()) {
               // Compare user input against the array of traits to see
@@ -129,13 +142,13 @@
               traitIndex = i;
               break;
             }
-            
+
             if (availableTrait[i].toLowerCase().indexOf(txtField.val().toLowerCase()) > -1) {
               // Get traits with keywords in it.
-              traitsWithKey.push(availableTrait[i]);  
+              traitsWithKey.push(availableTrait[i]);
             }
           }
-        
+
           // Process result of comparison.
           if(traitIndex != null) {
             // If search is successful - add reset link
@@ -174,7 +187,7 @@
                 break;
               }
             }
-        
+
             // When table is found.
             // Copy the table row <tr> with the trait information.
             var traitRow = $('#tabs table').find('tr').eq(traitTrIndex).html();
@@ -187,12 +200,12 @@
             if (traitsWithKey.length > 0) {
               // Found traits for suggestion.
               var suggestion = '';
-            
+
               for(var i = 0; i < traitsWithKey.length; i++) {
                 suggestion = suggestion + ' &bull;&nbsp;<a href="javascript:void();">'+traitsWithKey[i]+'</a>&nbsp;';
               }
               $('#container-search-result').append('<p id="list-suggest"><em>Did you mean:</em> ' + suggestion + '</p>');
-            
+
               // Enable links in did you mean:/suggested traits
               $('#list-suggest a').click(function() {
                 txtField.val($(this).text());
@@ -208,19 +221,19 @@
           }
         }
       });
-      
+
       // Remove search results and error messages.
       function removeElements() {
         // Remove any error messages from previous search.
-        if ($('div.messages').length > 0) { 
-          $('div.messages').remove(); 
-        } 
-        
-        // Remove any suggested links from previous search.  
+        if ($('div.messages').length > 0) {
+          $('div.messages').remove();
+        }
+
+        // Remove any suggested links from previous search.
         if ($('#list-suggest').length > 0) {
           $('#list-suggest').remove();
         }
-        
+
         // Remove any search result from previous search.
         if ($('#container-search-result table').length > 0) {
           $('#container-search-result table, #container-search-result em').hide('slow').remove();
@@ -229,28 +242,28 @@
         // Remove any search result from previous search.
         if ($('#container-search-result').length > 0) {
           $('#container-search-result').removeClass('search-on');
-        }        
+        }
 
         // Remove clear button from previous search.
         if ($('#lnk-reset').length > 0) {
           $('#lnk-reset').remove();
         }
       }
-      
+
       // Display error message.
       function errorMessage(title, message) {
         var error = '<div class="messages error">'+title+' '+message+'</div>';
         $('#container-search-result').append(error);
-        
+
         $('#edit-txt-search').focus();
       }
-      
+
       // Select project panel.
       var speed = 200;
       var panel = $('#container-sel-project');
       var linkProject = $('span a');
-      
-      linkProject.click(function(e) { 
+
+      linkProject.click(function(e) {
         e.preventDefault();
 
         if (panel.is(':hidden')) {
