4826decbe06a7787f02cf41bdbb1159bb46606f6
Reynold Tan <reynoldltan@gmail.com>
2017-01-09 13:28:11 -0600

    Updates 01.09.2017
    - Updated manage project interface to show backup files of data
    collectors (users)

--- theme/js/rawpheno.admin.script.js
+++ theme/js/rawpheno.admin.script.js
@@ -31,6 +31,61 @@
         if (!r)  return false;
       });
 
+
+      // Reveal validation result text.
+      $('div.container-cell').click(function() {
+        // Examine the height. When height is 50px, user wants to disclose the entire cell contents
+        // else, restore it to initial state.
+        var id = $(this).attr('id');
+        var i = id.replace(/vn-file-|vr-file-/, '');
+
+        var h = $(this).css('height');
+        if (h == '65px') {
+          $('#vr-file-' + i).css('height', '100%');
+
+          if ($('#vn-file-' + i)) {
+            $('#vn-file-' + i).css('height', '100%');
+          }
+        }
+        else {
+          $('#vr-file-' + i).css('height', '65px');
+
+          if ($('#vn-file-' + i)) {
+            $('#vn-file-' + i).css('height', '65px');
+          }
+        }
+      });
+
+
+      // Add event listener to user my folder.
+      $('.link-show-folder').click(function(e) {
+        e.preventDefault();
+
+        var lnk = $(this);
+        var attrId = lnk.attr('id');
+        var myFolder = '#show-' + attrId;
+
+        if (lnk.text() == '[Show]') {
+          lnk.text('[Hide]');
+          $(myFolder).show();
+
+          // Reset any view open.
+          var ls = $('.link-show-folder');
+          $.each(ls, function(key, value) {
+            var c = $(this);
+            var i = c.attr('id');
+            if (i != attrId) {
+              c.text('[Show]');
+              $('#show-' + i).hide();
+            }
+          });
+
+        }
+        else {
+          lnk.text('[Show]');
+          $(myFolder).hide();
+        }
+      });
     }
   };
 }(jQuery));
