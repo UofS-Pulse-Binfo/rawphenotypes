235df8626b937b855b4f08488e705c5b160ff32c
Reynold Tan <reynoldltan@gmail.com>
2017-10-18 16:25:10 -0600

    Create a block for Rawphenotypes pages in Issue #21
    - Create a menu block that contains links to raw phenotypes pages.

---
+++ theme/js/rawpheno.notification.block.script.js
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * Manage behavior in notification block.
+ */
+(function($) {
+  Drupal.behaviors.notificationBlock = {
+    attach: function (context, settings) {
+      // Reference elements.
+      var helpLink = $('#rawpheno-notification-need-help a');
+      var helpTopic = $('#rawpheno-notification-help-topics');
+      var myDashboard = $('#rawpheno-notification-dashboard');
+      var whyBackupLink = $('#rawpheno-notification-why-backup-link');
+      var whyBackupInfo = $('#rawpheno-notification-info-why-backup');
+      var okBackup = $('#rawpheno-notification-ok-button');
+
+      // Need Help?
+      helpLink.click(function(e) {
+        e.preventDefault();
+
+        var btn = $(this);
+        if (btn.text() == 'Close Help') {
+          btn.text('Need Help?');
+
+          helpTopic.slideUp().hide()
+          myDashboard.slideDown();
+        }
+        else {
+          btn.text('Close Help');
+
+          myDashboard.slideUp().hide();
+          helpTopic.slideDown()
+        }
+      });
+
+      // Why Backup?
+      whyBackupLink.click(function(e) {
+        e.preventDefault();
+
+        whyBackupInfo.show().slideDown();
+      });
+
+      // Why Backup acknowledged.
+      okBackup.click(function(e) {
+        e.preventDefault();
+
+        whyBackupInfo.slideUp().hide();
+      });
+    }
+  };
+}(jQuery));
