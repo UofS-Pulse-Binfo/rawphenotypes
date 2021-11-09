3c143710d6a6d054b2bb7e622e0be01b38454a28
reynold tan <reynoldltan@gmail.com>
2021-11-09 13:09:01 -0600

    Default experiment type to Research Experiment

--- theme/js/rawpheno.admin.projectfilter.script.js
+++ theme/js/rawpheno.admin.projectfilter.script.js
@@ -12,18 +12,33 @@
       // Reference project seleft field.
       var fldExperiment = $('#admin-sel-project');
       // Copy of all project when page first loaded.
-      var allProject =  Drupal.settings.rawpheno.all_project;
-    
+      var allProject = Drupal.settings.rawpheno.all_project;
+      var allProjectKeys = Object.keys(allProject);
+      // Default type - Research Experiment.
+      var defaultType = Drupal.settings.rawpheno.default_type;
+      
+      if (defaultType > 0) {
+        $('#admin-sel-types option[value="'+ defaultType +'"]').attr('selected', true);
+        populateSelect(defaultType);
+      }
+
       fldExperimentTypes.change(function(e) {
         // Seleted type.
         var typeVal = parseInt(e.target.value);
-        
+        populateSelect(typeVal);
+      });
+
+
+      /**
+       * Repopulate select field. 
+       */
+      function populateSelect(typeVal) {
         // RESET OPTIONS.
         $('#admin-sel-project option').remove();
 
         // CONSTRUCT NEW SET OF OPTIONS.
         // Filtered/all projects.
-        var exp = (typeVal > 0) ? typesProject[ typeVal ] : Object.keys(allProject);
+        var exp = (typeVal > 0) ? typesProject[ typeVal ].sort() : allProjectKeys;
                 
         if (exp.length > 0) {
           // First option is a blank.
@@ -34,7 +49,7 @@
           }));
           
           // Experiments options.
-          exp.reverse().forEach(function(i) {
+          exp.forEach(function(i) {
             i = parseInt(i);
 
             if (allProject[ i ]) {
@@ -45,6 +60,6 @@
             }
           });
         }
-      });
+      }
       // 
 }};}(jQuery));
