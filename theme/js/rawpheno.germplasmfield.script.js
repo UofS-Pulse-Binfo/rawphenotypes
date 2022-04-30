3ef128bc8e994684684c6486968c527622ab5ea2
reynoldltan@gmail.com <reynoldltan@gmail.com>
2022-04-30 17:00:14 -0600

    Adds more download options. @TODO: prepare export data

--- theme/js/rawpheno.germplasmfield.script.js
+++ theme/js/rawpheno.germplasmfield.script.js
@@ -17,8 +17,9 @@
           $('#rawphenotypes-define-raw-container').slideUp(200);
         });
       }
-      
-
+    
+      // Default select fields to Location + Experiment filter.
+      selectOptions('le'); 
       var imgOpacity = 0.5; 
       
       // Select box event - prepare link to download selection.
@@ -32,12 +33,7 @@
 
         // Reset other select to allow only one select field
         // at a time to export.
-        selects.each(function() { 
-          if ($(this).attr('id') != selectId) {
-            $('#' + $(this).attr('id') + '-img').css('opacity', '0.5');
-            this.selectedIndex = 0;
-          }
-        });
+        selectReset(selectId);
 
         if (selectValue == '0') {
           // None selected - default to select an option.
@@ -57,7 +53,6 @@
 
       // Listen to images clicked to launch data download.
       $('#rawphenotypes-germplasm-field-table td:last-child img').click(function(e) {
-        var imgId = e.target.id;
         var imgOpacity = $(this).css('opacity');
 
         if (imgOpacity == 1) {
@@ -66,6 +61,19 @@
             '_blank'
           );
         }
+      });
+
+      // Image to download ALL for a trait.
+      $('#rawphenotypes-germplasm-field-table td:first-child img').click(function(e) {
+        var imgId = e.target.id;
+        var i = imgId.split('-');
+        // rawphenotypes-germplasm-field-filterby-%s-img
+        var downloadLink = 't=' + i[4] + '&p=All&l=All&g=' + Drupal.settings.rawpheno.germ;
+
+        window.open(
+          Drupal.settings.rawpheno.exportLink + '?code=' + btoa(downloadLink),
+          '_blank'
+        );
       });
 
       // Listen to controls search, expand and select by.
@@ -147,7 +155,107 @@
         }); 
       
       // Select by type.
+      $('#rawphenotypes-germplasm-controls-selectby a').click(function(e) { 
+        e.preventDefault();  
 
+        var selByStates = ['Experiment', 'Location + Experiment'];
+        
+        // If link is experiment - switch it to location + experiment
+        // and vice versa.
+        var selByText = ($(this).text() == 'Experiment') ? 1 : 0;
+        var selByCur  = (selByText) ? 0 : 1; 
+        
+        $(this).text(selByStates[ selByText ]);
+        $(this).parent().find('span').text(selByStates[ selByCur ]);
+        
+        // Match selet field.
+        selectReset();
+        var sel = (selByCur) ? 'le' : 'e';  
+        selectOptions(sel);
+      });
 
+      /**
+       * Reset select boxes and export link.
+       * 
+       * @param selectId 
+       *   Trait id number used to reference a select field
+       *   If this is specified, exclude this select from reset operation. 
+       */
+      function selectReset(selectId = -1) {
+        var selects = $('#rawphenotypes-germplasm-field-table select');
+
+        selects.each(function() { 
+          if ($(this).attr('id') != selectId) {
+            $('#' + $(this).attr('id') + '-img').css('opacity', '0.5');
+            this.selectedIndex = 0;
+          }
+        });
+      }
+      
+      /**
+       * Remove option element from a select field.
+       * @param select
+       *   Object, reference to select element. 
+       */
+      function removeOptions(select) {
+        select.find('option').each(function(i, v) {
+          if (i > 0) $(this).remove();
+        });
+      }
+
+      /**
+       * Create select options
+       * 
+       * @param set
+       *   String, indicate if options is for location+experiment or experiment.
+       *   Default to le = location + experiment.
+       */
+      function selectOptions(set = 'le') {
+        var dataset = Drupal.settings.rawpheno;
+        var germplasm = dataset.germ;
+
+        // Prepare dataset.
+        if (set == 'le') {
+          // LOCATION + EXPERIMENT:
+          $.each(dataset.germRawdata, function(index, value){
+            var trait = index.split('_');
+            var element = $('#rawphenotypes-germplasm-field-filterby-' + trait[1]);
+            removeOptions(element);
+            $.each(value, function(i, v) {
+              var disabled = v['phenotype_customfield_terms:user_experiment'] == 1 ? '' : 'disabled'; 
+              element.append($('<option>', { 
+                value: trait[1] + '#' + v['phenotype_customfield_terms:id'] + '#' + v['phenotype_customfield_terms:location'] + '#' + germplasm, 
+                text : v['phenotype_customfield_terms:location'].toUpperCase() + '/' + v['phenotype_customfield_terms:name'],
+                disabled: disabled
+              }));
+            });
+          });
+        }
+        else if(set == 'e') {
+          // EXPERIMENT:
+          $.each(dataset.germRawdata, function(index, value){
+            var expCache = new Array();
+            var trait = index.split('_');
+            var element = $('#rawphenotypes-germplasm-field-filterby-' + trait[1]);
+            removeOptions(element);
+            $.each(value, function(i, v) {
+              var expId = v['phenotype_customfield_terms:id'];
+              if (!expCache.includes(expId)) {
+                var l = value.filter(function(v) { return v['phenotype_customfield_terms:id'] == expId });
+                var ql = l.map(function(v) { return v['phenotype_customfield_terms:location']; });
+                expCache.push(expId);
+                
+                var disabled = v['phenotype_customfield_terms:user_experiment'] == 1 ? '' : 'disabled';
+                element.append($('<option>', { 
+                  value: trait[1] + '#' + v['phenotype_customfield_terms:id'] + '#' + ql.join('+') + '#' + germplasm, 
+                  text : v['phenotype_customfield_terms:name'] + ' (' + l.length + ' Locations)',
+                  title: ql.join(' + '),
+                  disabled: disabled
+                }));
+              } 
+            });
+          });
+        }
+      }
 
 }};}(jQuery));
